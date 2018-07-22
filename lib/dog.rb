1631b3ee9ab00c21fb9f91193b12e920a4158a24
ma539@cornell.edu <ma539@cornell.edu>
2018-07-22 22:26:27 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  def initialize 
+  
+  def name 
+    @name 
+  end 
+  
+  def name=(input)
+    @name = input
+  end 
+  
+  def breed 
+    @breed = breed
+  end 
+  
+  def breed=(type)
+    @breed = type 
+  end 
+  
+  
+end 
